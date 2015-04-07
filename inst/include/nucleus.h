497077733c64fc0a9dd34112cfd8172b7c22a4a5
Romain François <romain@r-enthusiasts.com>
2015-04-07 17:25:41 +0200

    work around differnce between Rcpp and Rcpp1* about creating vectors with uninitialized data

--- inst/include/nucleus.h
+++ inst/include/nucleus.h
@@ -1,6 +1,18 @@
 #ifndef NUCLEUS_H
 #define NUCLEUS_H
 
+#if defined(Rcpp_hpp)
+    #define HAS_RCPP_IMPL
+    
+    // version for Rcpp
+    #define NEW_VECTOR(_Vector_,_name_,_n_) _Vector_ _name_ = no_init(_n_)  
+#endif
+
+#if defined(Rcpp11_h) or defined(Rcpp14_h)
+    #define HAS_RCPP_IMPL
+    #define NEW_VECTOR(_Vector_,_name_,_n_) _Vector_ _name_(_n_)
+#endif    
+    
 #include <RcppParallel.h>
 #include <nucleus/nucleus.h>
 
