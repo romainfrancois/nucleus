89d7157e2c75b807b5b455fa9f55331a62be1b7f
Romain François <romain@r-enthusiasts.com>
2015-04-07 17:44:27 +0200

    various fixes

--- inst/include/nucleus.h
+++ inst/include/nucleus.h
@@ -5,7 +5,7 @@
     #define HAS_RCPP_IMPL
     
     // version for Rcpp
-    #define NEW_VECTOR(_Vector_,_name_,_n_) _Vector_ _name_ = no_init(_n_)  
+    #define NEW_VECTOR(_Vector_,_name_,_n_) _Vector_ _name_ = Rcpp::no_init(_n_)  
 #endif
 
 #if defined(Rcpp11_h) or defined(Rcpp14_h)
--- inst/include/nucleus/extract.h
+++ inst/include/nucleus/extract.h
@@ -8,10 +8,10 @@ namespace nucleus {
         template <typename InputIterator, typename OutputIterator, typename IndexIterator>
         struct extractor {
             
-            extractor( InputIterator begin_, OutputIterator out_, IndexIterator index_ ): 
+            extractor( InputIterator data_, OutputIterator out_, IndexIterator index_ ): 
                 data(data_), out(out_), index(index_){}
                 
-            inline void operator()( const tbb::blocked_range<int>& r ){
+            inline void operator()( const tbb::blocked_range<int>& r ) const {
                 int start = r.begin(), end = r.end() ;
                 for(int i=start; i<end; i++){
                     out[i] = data[index[i] - 1] ;      
@@ -28,16 +28,16 @@ namespace nucleus {
     template <typename InputIterator, typename OutputIterator, typename IndexIterator>
     inline void extract( InputIterator begin, InputIterator end, OutputIterator out, IndexIterator index_begin, IndexIterator index_end ){
         int n = std::distance(index_begin, index_end ) ; 
-        parallel_for( 
-            blocked_range<int>(0, n), 
-            impl::extractor<InputIterator, OutputIterator, IndexIterator>( begin, out, index_begin )
-        ) ; 
+        typedef typename impl::extractor<InputIterator, OutputIterator, IndexIterator> Extractor ;
+        
+        Extractor ext( begin, out, index_begin ) ;
+        parallel_for( tbb::blocked_range<int>(0, n), ext ) ; 
     }
       
     
     #if defined(HAS_RCPP_IMPL)
     template <typename Vector>
-    inline Vector extract( const Vector& data, Rcpp::IntegerVector index ){
+    inline Vector extract( Vector& data, Rcpp::IntegerVector index ){
         NEW_VECTOR(Vector,res,index.size()) ;
         extract( data.begin(), data.end(), res.begin(), index.begin(), index.end() ) ;
         return res ;
